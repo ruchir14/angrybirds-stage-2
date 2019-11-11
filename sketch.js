07f09ffc813846e4cd5e7b7ca2e5ea3104089554
ruchir14 <56106833+ruchir14@users.noreply.github.com>
2019-11-11 19:38:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,8 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingshot;
-
+var bird, slingShot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -37,17 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
-   // log6 = new Log(230,180,80, PI/10);
-   slingshot = new Slingshot(bird.body,{x:200,y:100});
-    
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    strokeWeight(4);
+    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -65,14 +63,21 @@ function draw(){
 
     bird.display();
     platform.display();
+    //log6.display();
     slingshot.display();    
 }
 
-function mouseDragged() {
-Matter.Body.setPosition(bird.body,{x: mouseX,y: mouseY});
-
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
+
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
