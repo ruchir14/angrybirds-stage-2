770343feb7b5ec5315e9ec5f6e0fdb6445e4b493
ruchir14 <56106833+ruchir14@users.noreply.github.com>
2019-11-18 19:58:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,3 @@
-var arr1 = [1,2,3,4,5];
-console.log(arr1);
-
-var arr2 = ["Ruchir",14,true];
-console.log(arr2);
-
-var arr3 = [[1,2],[2,3],[3,4]];
-console.log(arr3);
-
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -18,10 +9,11 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg = "sprites/bg1.png";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -55,18 +47,27 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -80,10 +81,11 @@ function draw(){
 }
 
 function mouseDragged(){
-    if(gameState!=="launched"){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
-}
+
 
 function mouseReleased(){
     slingshot.fly();
@@ -92,6 +94,24 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-    //    slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
